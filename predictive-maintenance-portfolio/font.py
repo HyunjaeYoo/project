f1158132371dee645ec28d775f6f377dcb140046
silverains2-png <silverains2@gmail.com>
2026-09-02 15:24:41 +0900

    프로젝트

---
+++ predictive-maintenance-portfolio/font.py
@@ -0,0 +1,13 @@
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+
+names = {f.name for f in fm.fontManager.ttflist}
+for cand in ["AppleGothic", "Malgun Gothic", "NanumGothic", "NanumBarunGothic"]:
+    if cand in names:
+        plt.rcParams["font.family"] = cand
+        break
+    else:
+        print("[WARN] 한글 폰트를 찾지 못했습니다.")
+    plt.rcParams["axes.unicode_minus"] = False  # 마이너스 기호 깨짐 방지
+
+plt.title("한글폰트테스트")
